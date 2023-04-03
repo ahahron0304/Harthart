231e13145afd76a57713a38a64ad1a940d04a760
ahahron0304 <77768052+ahahron0304@users.noreply.github.com>
2023-04-03 17:22:13 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,53 @@
+function init() {
+  document.getElementById("click-me").addEventListener("click", showImagePage);
+}
+
+function showImagePage() {
+  document.getElementById("start-screen").style.display = "none";
+  document.getElementById("image-page").style.display = "block";
+  playAudio();
+  document.getElementById("background").classList.add("show"); // add show class to background element
+  positionHearts();
+}
+
+function playAudio() {
+  const audioElement = document.getElementById("audio");
+  audioElement.play();
+}
+
+function positionHearts() {
+  const scaleFactor = 15;
+
+  const heartShape = document.getElementById("heart-shape");
+  let index = 0;
+
+  function addHeart() {
+    if (index >= 100) {
+      alert("Ambangis mo idol!");
+      return;
+    }
+
+    const i = (index / 100) * 2 * Math.PI;
+    const heartEmoji = document.createElement("span");
+    heartEmoji.classList.add("heart-emoji");
+    heartEmoji.textContent = "❤️";
+
+    const x = 16 * Math.pow(Math.sin(i), 3);
+    const y = -(
+      13 * Math.cos(i) -
+      5 * Math.cos(2 * i) -
+      2 * Math.cos(3 * i) -
+      Math.cos(4 * i)
+    );
+
+    heartEmoji.style.top = `calc(35% - ${-y * scaleFactor}px)`;
+    heartEmoji.style.left = `calc(50% + ${x * scaleFactor}px)`;
+
+    heartShape.appendChild(heartEmoji);
+
+    index++;
+    setTimeout(addHeart, 770);
+  }
+
+  addHeart();
+}
